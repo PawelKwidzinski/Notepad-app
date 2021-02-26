a6af654282e9faeefdb231ba9e566836ce6f8f1c
PawelKwidzinski <solvap83@gmail.com>
2021-02-26 11:34:27 +0100

    Initial commit

---
+++ src/main/java/kwidzinski/pl/taskw8_2/Taskw82Application.java
@@ -0,0 +1,13 @@
+package kwidzinski.pl.taskw8_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taskw82Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taskw82Application.class, args);
+    }
+
+}
---
+++ src/test/java/kwidzinski/pl/taskw8_2/Taskw82ApplicationTests.java
@@ -0,0 +1,13 @@
+package kwidzinski.pl.taskw8_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taskw82ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
