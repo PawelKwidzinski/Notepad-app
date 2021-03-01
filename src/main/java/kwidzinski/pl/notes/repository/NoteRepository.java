664efa9090b7c9825cecc7f73abdb54e48f7ee3a
PawelKwidzinski <solvap83@gmail.com>
2021-03-01 13:48:10 +0100

    Repository for "Notes"

---
+++ src/main/java/kwidzinski/pl/notes/repository/NoteRepository.java
@@ -0,0 +1,9 @@
+package kwidzinski.pl.notes.repository;
+
+import kwidzinski.pl.notes.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
