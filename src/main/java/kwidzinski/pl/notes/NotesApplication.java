655af7664e4365ef84695aae0b27b3cecd88e25d
PawelKwidzinski <solvap83@gmail.com>
2021-03-01 13:45:35 +0100

    Renamed from Taskw82

--- src/main/java/kwidzinski/pl/notes/NotesApplication.java
+++ src/main/java/kwidzinski/pl/notes/NotesApplication.java
@@ -1,13 +1,13 @@
-package kwidzinski.pl.taskw8_2;
+package kwidzinski.pl.notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Taskw82Application {
+public class NotesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Taskw82Application.class, args);
+        SpringApplication.run(NotesApplication.class, args);
     }
 
 }
--- src/test/java/kwidzinski/pl/taskw8_2/NotesApplicationTests.java
+++ src/test/java/kwidzinski/pl/taskw8_2/NotesApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Taskw82ApplicationTests {
+class NotesApplicationTests {
 
     @Test
     void contextLoads() {
