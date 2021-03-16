17250cdbd75f2f2477118d1920314e865a046a81
PawelKwidzinski <solvap83@gmail.com>
2021-03-16 13:38:00 +0100

    Refactored method: "should_return_all_notes()" and Added methods: "should_add_given_note()", "should_find_note_by_id()", "should_delete_note()"

--- src/test/java/kwidzinski/pl/notes/service/NoteServiceUnitTest.java
+++ src/test/java/kwidzinski/pl/notes/service/NoteServiceUnitTest.java
@@ -17,10 +17,11 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
@@ -32,39 +33,69 @@ class NoteServiceUnitTest {
     @InjectMocks
     private NoteService noteService;
 
-    @BeforeEach
-    private void prepare_data() {
-        Note note1 = new Note("Title 1", "Description 1", LocalDate.of(2020,5,25));
+    private List<Note> testNoteList() {
+        Note note1 = new Note("Title 1", "Description 1", LocalDate.of(2020, 5, 25));
         note1.setId(1L);
-        Note note2 = new Note("Title 2", "Description 2", LocalDate.of(2021,1,25));
+        Note note2 = new Note("Title 2", "Description 2", LocalDate.of(2021, 1, 25));
         note2.setId(2L);
-        List<Note> testNotes = Arrays.asList(note1, note2);
-        Mockito.when(noteRepository.findAll()).thenReturn(testNotes);
+        return Arrays.asList(note1, note2);
     }
 
     @Test
-    void should_find_all_notes() {
+    void should_return_all_notes() {
         //given
-        prepare_data();
+        List<Note> testNotes = testNoteList();
+        given(noteService.findAllNotes()).willReturn(testNotes);
 
         //when
-        List<Note> allNotes = noteService.findAllNotes();
+        List<Note> expectedNotes = noteService.findAllNotes();
 
-        Assertions.assertEquals(1L, allNotes.get(0).getId());
-        Assertions.assertEquals("Title 1", allNotes.get(0).getTitle());
-        Assertions.assertEquals("Description 1", allNotes.get(0).getDescription());
-        Assertions.assertEquals(LocalDate.of(2020,5,25), allNotes.get(0).getDate());
+        //then
+       assertEquals(expectedNotes, testNotes);
+       verify(noteRepository).findAll();
     }
 
     @Test
-    void addNote() {
+    void should_add_given_note() {
+        // given
+        Note note3 = new Note("Title 3", "Description 3", LocalDate.of(2005, 11, 25));
+        note3.setId(3L);
+
+        // when
+        when(noteRepository.save(any(Note.class))).thenReturn(note3);
+
+        // then
+        Optional<Note> actual = noteService.addNote(note3);
+        Assertions.assertSame(note3, actual.get());
+        verify(noteRepository).save(note3);
     }
 
     @Test
-    void findById() {
+    void should_find_note_by_id() {
+        // given
+        Note note4 = new Note("Title 4", "Description 4", LocalDate.of(2008, 5, 25));
+        note4.setId(4L);
+
+        // when
+        when(noteRepository.findById(4L)).thenReturn(Optional.of(note4));
+
+        // then
+        final Optional<Note> actual = noteService.findById(4L);
+        Assertions.assertEquals(4L, actual.get().getId());
+        verify(noteRepository).findById(note4.getId());
     }
 
     @Test
-    void deleteById() {
+    void should_delete_note() {
+        // given
+        Note note5 = new Note("Title 5", "Description 5", LocalDate.of(2000, 4, 20));
+        note5.setId(5L);
+
+        // when
+        when(noteRepository.findById(note5.getId())).thenReturn(Optional.of(note5));
+
+        // then
+        noteService.deleteById(note5.getId());
+        verify(noteRepository).deleteById(note5.getId());
     }
 }
